5a6ef45cbc4278f355b64039be25c9e1c57dd3e4
sadra <amirsadra.namavar@gmail.com>
2023-01-25 01:04:34 +0330

    update related field in bibook serializer

--- backend/core/bibook/serializers.py
+++ backend/core/bibook/serializers.py
@@ -1,11 +1,18 @@
 from rest_framework import serializers
-from bibook.models import Book,Category
+from bibook.models import Book, Category
+
+
+class CategoryListingField(serializers.RelatedField):
+    def to_representation(self, value):
+        return {'id':value.id, 'title':value.title}
 
 
 class BookSerializer(serializers.ModelSerializer):
+    category = CategoryListingField(many=True, read_only=True)
+
     class Meta:
         model = Book
-        fields = ["id","title", "author", "translator", "about", "language","length",
+        fields = ["id", "title", "author", "translator", "about", "language", "length",
                   "publisher", "file_format", "file_size", "file_url", "category"]
         read_only_fields = ["view", "liked", "saved", "download"]
 
