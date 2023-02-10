54cbd1197f7c8d0bfc39f37f05c3ad1dc2eea2b2
sadra <amirsadra.namavar@gmail.com>
2023-02-10 07:21:45 +0330

    link book preview to book detail

--- frontend/app/src/components/index/home/category/books/book/book.jsx
+++ frontend/app/src/components/index/home/category/books/book/book.jsx
@@ -1,11 +1,17 @@
 import "./book.css";
+import { Link } from "react-router-dom";
 
 const Book = ({ props }) => {
-
   return (
     <>
       <div className="mb-3">
-        <img className="book-image ms-4 m-2 shadow" src={props.image_url} alt={props.title} />
+        <Link to={`/book/${props.id}`}>
+          <img
+            className="book-image ms-4 m-2 shadow"
+            src={props.image_url}
+            alt={props.title}
+          />
+        </Link>
         <span className="text-center d-block  fs-5">{props.title}</span>
       </div>
     </>
