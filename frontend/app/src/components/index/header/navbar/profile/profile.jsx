141cd16d167b8a39cef4f760834d31e88b32f058
sadra <amirsadra.namavar@gmail.com>
2023-02-26 13:31:11 +0330

    link login page to home navbar

--- frontend/app/src/components/index/header/navbar/profile/profile.jsx
+++ frontend/app/src/components/index/header/navbar/profile/profile.jsx
@@ -1,7 +1,8 @@
+import { Link } from "react-router-dom"
 
 
 const Profile = () => {
-    return  <h3 className="px-3 mt-1 me-3 nav-item">login</h3>
+    return  <Link to={"/account/login"} className="px-3  fs-4 mb-2 link mb-2 me-3 nav-item">ورود</Link>
   } 
   
   export default Profile
