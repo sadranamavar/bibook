62014bf2d769f99e4323c4c425bda382cf8b112d
sadra <amirsadra.namavar@gmail.com>
2023-02-28 17:57:37 +0330

    make dynamic login button in headers

--- frontend/app/src/components/index/header/navbar/profile/profile.jsx
+++ frontend/app/src/components/index/header/navbar/profile/profile.jsx
@@ -1,8 +1,14 @@
-import { Link } from "react-router-dom"
-
+import { Link } from "react-router-dom";
 
 const Profile = () => {
-    return  <Link to={"/account/login"} className="px-3  fs-4 mb-2 link mb-2 me-3 nav-item">ورود</Link>
-  } 
-  
-  export default Profile
+  const [title, link] = localStorage.getItem("JWT")
+    ? ["پیشخان", "/dashboard"]
+    : ["ورود", "/account/login"];
+  return (
+    <Link to={link} className="px-3  fs-4 mb-2 link mb-2 me-3 nav-item">
+      {title}
+    </Link>
+  );
+};
+
+export default Profile;
